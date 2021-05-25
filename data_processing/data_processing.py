7185b09ca52d3dc6352fa4ec8c09c5b97eac626e
Kyrylo Barkalov <sirkirill@gmail.com>
2021-05-25 08:53:58 +0300

    Add data processing

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -1,6 +1,30 @@
+
 from pyspark import SparkConf, SparkContext
-conf = SparkConf.setAppName("Data_processor").setMaster("local")
-sc = SparkContext(conf=conf)
+from pyspark.sql import SparkSession
 
-distfile = sc.textFile("data.txt")
 
+def start_spark(app_name):
+    builder = SparkSession.builder.master(master='local').appName(app_name)
+
+    return builder
+
+
+def extract_data(spark):
+    data_frame = spark.read.load('file:///Users/kbarkalov/PycharmProjects/Stepik_data_process/data_processing/data.json', format='json')
+
+    return data_frame
+
+
+def collect_n_max_value(n, data_frame):
+    df_transform = data_frame.orderBy('learners_count', ascending=False).limit(5)
+
+    return df_transform
+
+
+spark_builder = start_spark('Stepik_processing')
+spark_sess = spark_builder.getOrCreate()
+
+df = extract_data(spark_sess)
+top5 = collect_n_max_value(5, df)
+top5.write.format('json').save('file:///Users/kbarkalov/PycharmProjects/Stepik_data_process/data_processing/data_top')
+top5.show()
